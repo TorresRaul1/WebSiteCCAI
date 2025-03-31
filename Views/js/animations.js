b4c8948e8cb2c03f935873949f6ac54e56fe5f24
IsabelPopoca <2021452125@teschi.edu.mx>
2025-03-31 11:41:10 -0600

    <Add:Dev> Corrección del mensaje de enviado

--- Views/js/animations.js
+++ Views/js/animations.js
@@ -3,33 +3,16 @@ document.addEventListener("DOMContentLoaded", function () {
     AOS.init();
 });
 
-document.addEventListener("DOMContentLoaded", function () {
-    const message = "Hemos recibido tu solicitud, pronto nos pondremos en contacto contigo.¡Gracias por tu interés en nuestros servicios! 😊";
+document.getElementById("sendButton").addEventListener("click", function(event) {
+    event.preventDefault(); // Evita que el formulario recargue la página
     
-    const messageElement = document.getElementById("animatedText");
-    let index = 0;
+    var toastEl = document.getElementById("messageToast"); // Obtiene el Toast
+    var toast = new bootstrap.Toast(toastEl);
+    toast.show(); // Muestra el Toast
+});
 
-    function typeWriter() {
-        if (index < message.length) {
-            messageElement.innerHTML += message.charAt(index);
-            index++;
-            setTimeout(typeWriter, 50); // Ajusta la velocidad de la escritura
-        }
-    }
-
-document.getElementById("messageModal").addEventListener("shown.bs.modal", function () {
-    messageElement.innerHTML = ""; // Reiniciar texto
-    index = 0;
-    typeWriter();
+// Aplicar el estilo para quitar el borde a los inputs
+var inputs = document.querySelectorAll('input, textarea');
+inputs.forEach(function(input) {
+    input.style.border = 'none';
 });
-});
-document.getElementById('sendButton').addEventListener('click', function(event) {
-    event.preventDefault(); // Evita que el enlace redirija
-    var myModal = new bootstrap.Modal(document.getElementById('messageModal'));
-    myModal.show(); // Muestra el modal
-});
-    // Aplicar el estilo para quitar el borde a los inputs
-    var inputs = document.querySelectorAll('input, textarea');
-    inputs.forEach(function(input) {
-        input.style.border = 'none';
-    });
